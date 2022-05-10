d4a5dd9190ccace479ec361dd5c80362f9c51998
ramarinor <rinor.rama95@gmail.com>
2022-05-10 12:33:36 +0200

    update user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -11,6 +11,21 @@ export class UsersService {
   create(email: string, password: string) {
     const user = this.repo.create({ email, password });
 
-    return this.repo.save(user); 
+    return this.repo.save(user);
   }
+  findOne(id: number) {
+    return this.repo.findOne(id);
+  }
+  find(email: string) {
+    return this.repo.find({ email });
+  }
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new Error('User not found');
+    }
+    Object.assign(user, attrs);
+    return this.repo.save(user);
+  }
+  remove() {}
 }
